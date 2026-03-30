44c1ccb08f8a135e4d7001b3257cd521f3c72027
Nafeesath Raffa <nafeesathraffa8@gmail.com>
2026-03-30 22:56:16 +0530

    wrote the comment as clear explanaton and deleted unnecessary prints.

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
+#import libraries
 import pandas as pd
 from datetime import datetime
 
+#read the csv file
 df_csv = pd.read_csv('data/shipments.csv')
-print(df_csv)
 
+#count of shipment status
 status_counts = df_csv['status'].value_counts()
-print(status_counts)
+
+#coverted from series to table
 status_counts_reset = status_counts.reset_index()
 
-
+#total freight cost calculation
 total_fc = df_csv['freight_cost'].sum()
-print(total_fc)
+
+#converted the single number output to a DataFrame
 df_fc = pd.DataFrame({
           'Metric' : ['Total Freight Cost'],
           'Value' : [total_fc]
         })
 
+#converted the text dates to datetime objects
 today = datetime.today()
 converted_exp_del = pd.to_datetime(df_csv['expected_delivery'])
-overdue = df_csv[(converted_exp_del<today) & (df_csv['status']!='Delivered')]
-print(overdue)
 
+#filtered the overdue shipments
+overdue = df_csv[(converted_exp_del<today) & (df_csv['status']!='Delivered')]
+
+#excel file creation - full shipment data table
 df_csv.to_excel("output/shipments.xlsx", index=False)
 
+#excel file creation - summary of status count, total freight count and overdue list
 with pd.ExcelWriter("output/summary.xlsx") as writer:
   status_counts_reset.to_excel(writer, startrow=0, index=False)
   overdue.to_excel(writer, startrow=7, index=False)
