d683389bd5f59ebd6823e9ec12069ce37dbde444
Nafeesath Raffa <nafeesathraffa8@gmail.com>
2026-03-30 22:42:47 +0530

    write everything to excel. create two sheets for shipments and summary

--- main.py
+++ main.py
@@ -6,11 +6,25 @@ print(df_csv)
 
 status_counts = df_csv['status'].value_counts()
 print(status_counts)
+status_counts_reset = status_counts.reset_index()
+
 
 total_fc = df_csv['freight_cost'].sum()
 print(total_fc)
+df_fc = pd.DataFrame({
+          'Metric' : ['Total Freight Cost'],
+          'Value' : [total_fc]
+        })
 
 today = datetime.today()
 converted_exp_del = pd.to_datetime(df_csv['expected_delivery'])
 overdue = df_csv[(converted_exp_del<today) & (df_csv['status']!='Delivered')]
 print(overdue)
+
+df_csv.to_excel("output/shipments.xlsx", index=False)
+
+with pd.ExcelWriter("output/summary.xlsx") as writer:
+  status_counts_reset.to_excel(writer, startrow=0, index=False)
+  overdue.to_excel(writer, startrow=7, index=False)
+  df_fc.to_excel(writer, startrow=13, index=False)
+
